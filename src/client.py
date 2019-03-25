f86785d8a8adaa0c0b74238821d556c97aaa2f4e
SLITI Brahim <brahim.sliti@supinfo.com>
2019-03-25 11:42:33 +0100

    updating client.py for test purposes

--- src/client.py
+++ src/client.py
@@ -1,5 +1,15 @@
+from Block import Block
+from ProofOfWork import ProofOfWork
+
 def main():
-    print("it works")
+    block = Block()
+
+    proof_of_work = ProofOfWork.run(block)
+    block.set_proof_of_work(proof_of_work)
+    block.set_hash(proof_of_work.get_hash())
+
+    print("hash of the mined block: ", block.get_hash())
+    print(block.compute_hash())
 
 if __name__ == '__main__':
     main()
