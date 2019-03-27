223654c054a45a11dfee53ac45324b6b26071401
SLITI Brahim <brahim.sliti@supinfo.com>
2019-03-27 17:23:17 +0100

    set timestamp of a block

--- src/client.py
+++ src/client.py
@@ -1,6 +1,7 @@
 from Block import Block
 from GenesisBlock import GenesisBlock
 from ProofOfWork import ProofOfWork
+import time
 
 def main():
     block = GenesisBlock()
@@ -8,9 +9,11 @@ def main():
     proof_of_work = ProofOfWork.run(block)
     block.set_proof_of_work(proof_of_work)
     block.set_hash(proof_of_work.get_hash())
+    block.set_timestamp(time.time())
 
     print("hash of the mined block:", block.get_hash())
     print("nonce:", block.get_proof_of_work().get_nonce())
+    print(block.get_timestamp())
 
 if __name__ == '__main__':
     main()
