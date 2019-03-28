816bf9a6d87c29cd403bf4eb8c6eaaed8f2c4361
SLITI Brahim <brahim.sliti@supinfo.com>
2019-03-28 07:47:28 +0100

    implement Blockchain add_blocks method

--- src/Blockchain.py
+++ src/Blockchain.py
@@ -12,6 +12,9 @@ class Blockchain:
         return self.__chain[start:]
 
     def get_last_block(self):
+        if self.get_chain_length() == 0:
+            return None
+
         return self.__chain[-1]
 
     def get_chain_length(self):
@@ -28,6 +31,19 @@ class Blockchain:
         self.__chain.append(block)
 
         return self.__chain
+
+    def add_blocks_to_chain(self, obj_list):
+        is_block_instances = list(map(lambda obj: isinstance(obj, Block), obj_list))
+
+        # check if obj_list contains non-Block instances
+        if not all(is_block_instances):
+            raise Exception("List must contain only Block instances")
+
+        for block in obj_list:
+            try:
+                self.add_block_to_chain(block)
+            except Exception:
+                continue
 
     def find_block(self, str_hash):
         """Retrieve a block in the chain by its hash"""
