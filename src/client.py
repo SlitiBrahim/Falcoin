a9cd9d673e3681567a196dbea7f0346e9d767091
SLITI Brahim <brahim.sliti@supinfo.com>
2019-05-18 23:37:16 +0200

    update client sandbox

--- src/client.py
+++ src/client.py
@@ -59,14 +59,22 @@ def main():
     #
     # print(block)
 
+    blockchain = Blockchain()
 
     my_private_key, my_public_key = crypto.generate_key_pair()
-
     yanis_private_key, yanis_public_key = crypto.generate_key_pair()
 
     txs = [
         CoinbaseTransaction(CoinbaseTransaction.generate_default_output(yanis_public_key))
     ]
+
+    genesis_block = GenesisBlock(Transaction.extract_valid_transactions(txs))
+    pow = ProofOfWork.run(genesis_block)
+    genesis_block.set_proof_of_work(pow)
+    genesis_block.set_hash(pow.get_hash())
+    genesis_block.set_timestamp(time.time())
+
+    blockchain.add_block_to_chain(genesis_block)
 
     input = Input(txs[0], 0, txs[0].get_output(index=0))
     input.set_pubsig(Input.generate_pubsig(input, yanis_private_key))
@@ -76,7 +84,39 @@ def main():
         Transaction(inputs=[input], outputs=[Output(10, my_public_key)], fees=0.0),
     ]
 
-    valid = txs1[1].get_inputs()[0].is_valid()
+    block1 = Block(Transaction.extract_valid_transactions(txs1))
+    block1.set_index(1)
+    block1.set_prev_hash(genesis_block.get_hash())
+    pow = ProofOfWork.run(block1)
+    block1.set_proof_of_work(pow)
+    block1.set_hash(pow.get_hash())
+    block1.set_timestamp(time.time())
+
+    blockchain.add_block_to_chain(block1)
+
+    input = Input(txs1[1], 0, txs1[1].get_output(index=0))
+    input.set_pubsig(Input.generate_pubsig(input, my_private_key))
+
+    txs2 = [
+        CoinbaseTransaction(CoinbaseTransaction.generate_default_output(my_public_key)),
+        Transaction(inputs=[input], outputs=[Output(10, yanis_public_key)], fees=0.0),
+    ]
+
+    block2 = Block(Transaction.extract_valid_transactions(txs2))
+    block2.set_index(1)
+    block2.set_prev_hash(block1.get_hash())
+    pow = ProofOfWork.run(block2)
+    block2.set_proof_of_work(pow)
+    block2.set_hash(pow.get_hash())
+    block2.set_timestamp(time.time())
+
+    blockchain.add_block_to_chain(block2)
+
+    print(genesis_block)
+    print(block1)
+    print(block2)
+
+    # valid = txs1[1].get_inputs()[0].is_valid()
 
     valid_txs = Transaction.extract_valid_transactions(txs1)
 
