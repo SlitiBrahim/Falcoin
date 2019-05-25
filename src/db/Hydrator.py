67cda4abe1c3adb646e739c4d94c1f348046bd4c
SLITI Brahim <brahim.sliti@supinfo.com>
2019-05-25 17:26:35 +0200

    remove TODO comment

--- src/db/Hydrator.py
+++ src/db/Hydrator.py
@@ -8,7 +8,6 @@ class Hydrator:
         is_genesis_block = doc['index'] == 0
 
         if is_genesis_block:
-            # TODO: Implement deserialize method on GenesisBlock
             obj = GenesisBlock.deserialize(doc)
         else:
             obj = Block.deserialize(doc)
