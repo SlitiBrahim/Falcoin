7961629e5769840019739c000fe6790fe754d8ee
SLITI Brahim <brahim.sliti@supinfo.com>
2019-05-25 19:25:19 +0200

    update Repository.add method with some verification

--- src/db/Repository.py
+++ src/db/Repository.py
@@ -1,4 +1,5 @@
 from tinydb import TinyDB
+from blockchain import Block
 
 class Repository:
 
@@ -9,8 +10,20 @@ class Repository:
         if self.__db is None:
             self.__db = TinyDB(db_path)
 
-    def add(self, block):
-        self.__db.insert(block.serialize())
+    def add(self, obj):
+        """Add a block or a list of blocks"""
+        if obj is not list and isinstance(obj, Block) == False:
+            raise TypeError("arg should be a block obj or a list of block objects")
+
+        if isinstance(obj, Block):
+            objs = [obj]
+        else:
+            objs = obj
+
+        # serialized block objects
+        s_objs = [o.serialize() for o in objs]
+
+        self.__db.insert_multiple(s_objs)
 
     def get_all_docs(self):
         return self.__db.all()
