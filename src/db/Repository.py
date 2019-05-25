2753f297ac589f116a90784151a178c575cf503e
SLITI Brahim <brahim.sliti@supinfo.com>
2019-05-25 20:08:44 +0200

    implement sync_db method on Repository

--- src/db/Repository.py
+++ src/db/Repository.py
@@ -1,5 +1,6 @@
 from tinydb import TinyDB
 from blockchain import Block
+from .Hydrator import Hydrator
 
 class Repository:
 
@@ -12,7 +13,7 @@ class Repository:
 
     def add(self, obj):
         """Add a block or a list of blocks"""
-        if obj is not list and isinstance(obj, Block) == False:
+        if type(obj) is not list and isinstance(obj, Block) == False:
             raise TypeError("arg should be a block obj or a list of block objects")
 
         if isinstance(obj, Block):
@@ -27,3 +28,20 @@ class Repository:
 
     def get_all_docs(self):
         return self.__db.all()
+
+    def db_sync(self, blockchain):
+        """Sync db with in-memory blockchain.
+        Add in-memory blockchain blocks in db if they were not added
+        """
+
+        db_blocks = [Hydrator.hydrate_block(s_block) for s_block in self.get_all_docs()]
+
+        # if blockchain has more blocks than db, that means db is not updated
+        if len(blockchain.get_chain()) > len(db_blocks):
+            for index, bc_block in enumerate(blockchain.get_chain()):
+                try:
+                    # will raise Error if db_blocks don't have that index
+                    _ = db_blocks[index]
+                except IndexError:
+                    # if error caught that means db is missing that block
+                    self.add(bc_block)
