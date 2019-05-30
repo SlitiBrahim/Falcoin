b0b193fe43248213a3b09d26b42ad16fde9e2795
SLITI Brahim <brahim.sliti@supinfo.com>
2019-05-30 17:34:16 +0200

    update client.py by adding miner and change txs

--- src/client.py
+++ src/client.py
@@ -37,8 +37,9 @@ def main():
 
     print("run client as {} node.".format(args.node))
 
-    my_private_key, my_public_key = crypto.generate_key_pair()
+    br_private_key, br_public_key = crypto.generate_key_pair()
     yanis_private_key, yanis_public_key = crypto.generate_key_pair()
+    miner_private_key, miner_public_key = crypto.generate_key_pair()
 
     txs = [
         CoinbaseTransaction(CoinbaseTransaction.generate_default_output(yanis_public_key))
@@ -56,8 +57,8 @@ def main():
     input.set_pubsig(Input.generate_pubsig(input, yanis_private_key))
 
     txs1 = [
-        CoinbaseTransaction(CoinbaseTransaction.generate_default_output(my_public_key)),
-        Transaction(inputs=[input], outputs=[Output(10, my_public_key)], fees=0.0),
+        CoinbaseTransaction(CoinbaseTransaction.generate_default_output(miner_public_key)),
+        Transaction(inputs=[input], outputs=[Output(50, br_public_key), Output(50, yanis_public_key)]),
     ]
 
     block1 = Block(Transaction.extract_valid_transactions(txs1, blockchain))
@@ -71,11 +72,11 @@ def main():
     blockchain.add_block_to_chain(block1)
 
     input = Input(txs1[1].get_hash(), 0, txs1[1].get_output(index=0))
-    input.set_pubsig(Input.generate_pubsig(input, my_private_key))
+    input.set_pubsig(Input.generate_pubsig(input, br_private_key))
 
     txs2 = [
-        CoinbaseTransaction(CoinbaseTransaction.generate_default_output(my_public_key)),
-        Transaction(inputs=[input], outputs=[Output(10, yanis_public_key)], fees=0.0),
+        CoinbaseTransaction(CoinbaseTransaction.generate_default_output(miner_public_key)),
+        Transaction(inputs=[input], outputs=[Output(30, yanis_public_key), Output(20, br_public_key)], fees=0.0),
     ]
 
     block2 = Block(Transaction.extract_valid_transactions(txs2, blockchain))
@@ -88,22 +89,9 @@ def main():
 
     blockchain.add_block_to_chain(block2)
 
-    txs3 = [
-        CoinbaseTransaction(CoinbaseTransaction.generate_default_output(yanis_public_key)),
-        Transaction(inputs=[input], outputs=[Output(10, yanis_public_key)], fees=0.0),
-    ]
-
-    block3 = Block(Transaction.extract_valid_transactions(txs3, blockchain))
-    block3.set_index(3)
-    block3.set_prev_hash(block1.get_hash())
-    pow = ProofOfWork.run(block3)
-    block3.set_proof_of_work(pow)
-    block3.set_hash(pow.get_hash())
-    block3.set_timestamp(time.time())
-
-    blockchain.add_block_to_chain(block3)
-
-    print("my_pubkey", my_public_key)
+    print("br_pubkey", br_public_key)
+    print("yanis_pubkey", yanis_public_key)
+    print("miner_pubkey", miner_public_key)
 
     # my_txs = blockchain.get_transactions(my_public_key)
 
