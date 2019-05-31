37ec06e41257e153d27f80cf0941892b0c00e909
SLITI Brahim <brahim.sliti@supinfo.com>
2019-05-31 15:14:01 +0200

    commenting out blockchain logic in client.py

--- src/client.py
+++ src/client.py
@@ -42,61 +42,61 @@ def main():
     print("run client as {} node.".format(args.node))
     print("hard coded nodes:", ', '.join(args.hc_nodes))
 
-    br_private_key, br_public_key = crypto.generate_key_pair()
-    yanis_private_key, yanis_public_key = crypto.generate_key_pair()
-    miner_private_key, miner_public_key = crypto.generate_key_pair()
-
-    txs = [
-        CoinbaseTransaction(CoinbaseTransaction.generate_default_output(yanis_public_key))
-    ]
-
-    genesis_block = GenesisBlock(Transaction.extract_valid_transactions(txs, blockchain))
-    pow = ProofOfWork.run(genesis_block)
-    genesis_block.set_proof_of_work(pow)
-    genesis_block.set_hash(pow.get_hash())
-    genesis_block.set_timestamp(time.time())
-
-    blockchain.add_block_to_chain(genesis_block)
-
-    input = Input(txs[0].get_hash(), 0, txs[0].get_output(index=0))
-    input.set_pubsig(Input.generate_pubsig(input, yanis_private_key))
-
-    txs1 = [
-        CoinbaseTransaction(CoinbaseTransaction.generate_default_output(miner_public_key)),
-        Transaction(inputs=[input], outputs=[Output(50, br_public_key), Output(50, yanis_public_key)]),
-    ]
-
-    block1 = Block(Transaction.extract_valid_transactions(txs1, blockchain))
-    block1.set_index(1)
-    block1.set_prev_hash(genesis_block.get_hash())
-    pow = ProofOfWork.run(block1)
-    block1.set_proof_of_work(pow)
-    block1.set_hash(pow.get_hash())
-    block1.set_timestamp(time.time())
-
-    blockchain.add_block_to_chain(block1)
-
-    input = Input(txs1[1].get_hash(), 0, txs1[1].get_output(index=0))
-    input.set_pubsig(Input.generate_pubsig(input, br_private_key))
-
-    txs2 = [
-        CoinbaseTransaction(CoinbaseTransaction.generate_default_output(miner_public_key)),
-        Transaction(inputs=[input], outputs=[Output(30, yanis_public_key), Output(20, br_public_key)], fees=0.0),
-    ]
-
-    block2 = Block(Transaction.extract_valid_transactions(txs2, blockchain))
-    block2.set_index(1)
-    block2.set_prev_hash(block1.get_hash())
-    pow = ProofOfWork.run(block2)
-    block2.set_proof_of_work(pow)
-    block2.set_hash(pow.get_hash())
-    block2.set_timestamp(time.time())
-
-    blockchain.add_block_to_chain(block2)
-
-    print("br_pubkey", br_public_key)
-    print("yanis_pubkey", yanis_public_key)
-    print("miner_pubkey", miner_public_key)
+    # br_private_key, br_public_key = crypto.generate_key_pair()
+    # yanis_private_key, yanis_public_key = crypto.generate_key_pair()
+    # miner_private_key, miner_public_key = crypto.generate_key_pair()
+    #
+    # txs = [
+    #     CoinbaseTransaction(CoinbaseTransaction.generate_default_output(yanis_public_key))
+    # ]
+    #
+    # genesis_block = GenesisBlock(Transaction.extract_valid_transactions(txs, blockchain))
+    # pow = ProofOfWork.run(genesis_block)
+    # genesis_block.set_proof_of_work(pow)
+    # genesis_block.set_hash(pow.get_hash())
+    # genesis_block.set_timestamp(time.time())
+    #
+    # blockchain.add_block_to_chain(genesis_block)
+    #
+    # input = Input(txs[0].get_hash(), 0, txs[0].get_output(index=0))
+    # input.set_pubsig(Input.generate_pubsig(input, yanis_private_key))
+    #
+    # txs1 = [
+    #     CoinbaseTransaction(CoinbaseTransaction.generate_default_output(miner_public_key)),
+    #     Transaction(inputs=[input], outputs=[Output(50, br_public_key), Output(50, yanis_public_key)]),
+    # ]
+    #
+    # block1 = Block(Transaction.extract_valid_transactions(txs1, blockchain))
+    # block1.set_index(1)
+    # block1.set_prev_hash(genesis_block.get_hash())
+    # pow = ProofOfWork.run(block1)
+    # block1.set_proof_of_work(pow)
+    # block1.set_hash(pow.get_hash())
+    # block1.set_timestamp(time.time())
+    #
+    # blockchain.add_block_to_chain(block1)
+    #
+    # input = Input(txs1[1].get_hash(), 0, txs1[1].get_output(index=0))
+    # input.set_pubsig(Input.generate_pubsig(input, br_private_key))
+    #
+    # txs2 = [
+    #     CoinbaseTransaction(CoinbaseTransaction.generate_default_output(miner_public_key)),
+    #     Transaction(inputs=[input], outputs=[Output(30, yanis_public_key), Output(20, br_public_key)], fees=0.0),
+    # ]
+    #
+    # block2 = Block(Transaction.extract_valid_transactions(txs2, blockchain))
+    # block2.set_index(1)
+    # block2.set_prev_hash(block1.get_hash())
+    # pow = ProofOfWork.run(block2)
+    # block2.set_proof_of_work(pow)
+    # block2.set_hash(pow.get_hash())
+    # block2.set_timestamp(time.time())
+    #
+    # blockchain.add_block_to_chain(block2)
+    #
+    # print("br_pubkey", br_public_key)
+    # print("yanis_pubkey", yanis_public_key)
+    # print("miner_pubkey", miner_public_key)
 
     # my_txs = blockchain.get_transactions(my_public_key)
 
